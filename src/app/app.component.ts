511dbabc7b1469257e02c14ba8294688a749f714
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-07-30 15:02:06 +0300

    NGRX-1 add count code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ngrx-for-beginners';
+  public counter = 0;
+  public updateAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  public increase(): void {
+    this.updateAt = Date.now();
+    this.counter++;
+  }
+
+  public decrease(): void {
+    this.updateAt = Date.now();
+    this.counter--;
+  }
+
+  public clear(): void {
+    this.updateAt = Date.now();
+    this.counter = 0;
+  }
 }
